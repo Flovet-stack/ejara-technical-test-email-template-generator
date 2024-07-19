83fa2747d0249247452a2276fb3686e2dc21dbdb
Flovet-stack <lovetofloveto3@gnail.com>
2024-07-19 07:12:28 +0100

    added the download HTML feature

--- app/(dashboard)/templates/_components/html-preview/HtmlPreview.tsx
+++ app/(dashboard)/templates/_components/html-preview/HtmlPreview.tsx
@@ -7,18 +7,20 @@ import hljs from "highlight.js/lib/core";
 import html from "highlight.js/lib/languages/xml";
 import "highlight.js/styles/github.css";
 import "./html-preview.scss";
+import { CustomButton } from "@/shared/components";
 
 export const HtmlPreview = () => {
   const editorState = useAppSelector((store: RootState) => store.editor);
   const { dnd } = editorState;
 
   const [highlightedHtml, setHighlightedHtml] = useState("");
+  const [htmlString, setHtmlString] = useState("");
 
   hljs.registerLanguage("html", html);
 
   useEffect(() => {
     // Convert the nodes to an HTML string
-    const htmlString = render(
+    const generatedHtmlString = render(
       <Html lang="en">
         {dnd.nodes.map((node) => (
           <LoadComponentType key={node.id} node={node} />
@@ -28,14 +30,36 @@ export const HtmlPreview = () => {
     );
 
     // Highlight the HTML string
-    const highlighted = hljs.highlight(htmlString, { language: "html" }).value;
+    const highlighted = hljs.highlight(generatedHtmlString, {
+      language: "html",
+    }).value;
 
     // Set the highlighted HTML
     setHighlightedHtml(highlighted);
+    setHtmlString(generatedHtmlString);
   }, [dnd.nodes]);
+
+  const downloadHtml = () => {
+    const blob = new Blob([htmlString], { type: "text/html" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "preview.html";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
 
   return (
     <div className="html-preview">
+      <div className="flex justify-end mb-4">
+        <CustomButton
+          theme="black"
+          text="Download HTML"
+          onClick={downloadHtml}
+        />
+      </div>
       <pre>
         <code
           className="html"
